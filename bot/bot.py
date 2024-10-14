b432c41b3f830c1aedfe0c1981b863e87f137d0a
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-10-14 00:04:51 -0700

    startup event listner

--- bot/bot.py
+++ bot/bot.py
@@ -10,6 +10,13 @@ client=arc.GatewayClient(bot)
 
 client.load_extensions_from(path.join("bot", "extensions"))
 
+# it's midnight wtf am I writing
+# This code gets called write after the extensions get loaded
+@client.listen()
+async def on_startup(event: arc.StartedEvent):
+    # Jesus Fucking Christ, you have got to be kidding me
+    # This is not a 2012 minecraft video
+    print("Blazing new trails")
 
 
 
