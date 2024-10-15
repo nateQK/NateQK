b38d7e4cc9e0e181450ecf7006e3360ef1ae0efc
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-10-14 22:58:11 -0700

    groundwork for the environment file

--- bot/enviroment.py
+++ bot/enviroment.py
@@ -1,14 +1,53 @@
 from pydantic import BaseModel
 
-class MongoConfig:
+class MongoConfig(BaseModel):
+    """Connection info for the mongodb server"""
     HOST: str
     PORT: int
     USERNAME: str
     PASSWORD: str
 
 #? Pydnatic class for Verifying data
-class BaseConfig:
+class BaseConfig(BaseModel):
+    """
+    Base Config for the app
+    Any changes made to the config need to be reflected here
+    """
     TOKEN: str
     PREFIX: str
     database: MongoConfig
 
+
+class Config:
+    @classmethod
+    def loadConfig(cls):
+        """Loads, Fetches, and Validates config from a pre-determined file"""
+        pass
+    
+    class Mongo:
+
+        @classmethod
+        def getHost(cls):
+            """Fetches values of host from config"""
+            pass
+
+        @classmethod
+        def getPort(cls):
+            """Fetches values of port from config"""
+            pass
+
+        @classmethod
+        def getUsername(cls):
+            """Fetches values of username from config"""
+            pass
+
+    class normal:
+        @classmethod
+        def getToken(cls):
+            """Gets bot token from config"""
+            pass
+
+        @classmethod
+        def getPrefix(cls):
+            """Gets bot prefix from config"""
+            pass
