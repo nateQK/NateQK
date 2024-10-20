83a46d53f7144a0aabb5571beb2d1ed7aa6d06aa
RaidyQK <thisisraidyqk@gmail.com>
2024-10-20 20:34:45 +0300

    Forgot smth

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -2,5 +2,5 @@ from .environment import Config
 
 Config.loadConfig()
 
-configBOT = Config.Bot()
-configDB = Config.Database()
+configBOT: Config.Bot = Config.Bot()
+configDB: Config.Database = Config.Database()
