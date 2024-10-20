45a79be6fbd88c96186bdb0ac4d9647616d8f54e
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-10-20 16:44:29 -0700

    auto init db on bot start

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,6 +1,11 @@
+import asyncio
 from .environment import Config
 
 Config.loadConfig()
 
 configBOT: Config.Bot = Config.Bot()
 configDB: Config.Database = Config.Database()
+
+from .database import Database
+
+asyncio.run(Database.Connect())
