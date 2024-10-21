064a79dd848ab7ca5eeebde1020ffc179e0741e9
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-10-21 09:35:49 -0700

    WARNING: THIS BREAKS THE BOTadded a way to load using pydantic and validate
    Reworked so much code to allow for type safety

--- bot/utils/environment.py
+++ bot/utils/environment.py
@@ -2,71 +2,86 @@
 import tomllib
 from os import path
 from loguru import logger
-from munch import munchify
+from pydantic import BaseModel
+from typing import TypedDict
+
+
+class DefaultConfig(BaseModel):
+    token: str
+
+class DatabaseConfig(BaseModel):
+    host: str
+    port: int
+    username: str
+    password: str
+
+class VersionConfig(BaseModel):
+    version: str
+
+class Configuration(BaseModel):
+    DEFAULT: DefaultConfig
+    DATABASE: DatabaseConfig
+    VERSION: VersionConfig
+
+
 
 class Config:
+    config: Configuration
+
     @classmethod
-    def loadConfig(cls):
+    def loadConfig(cls) -> None:
         """Loads, Fetches, and Validates config from a pre-determined file"""
 
-        configfile: str = path.join("bot", "app.toml")
+        #configfile: str = path.join("bot", "app.toml")
+
+        #? Dev configfile
+        configfile: str = path.join("..", "app.toml")
 
 
         with open(configfile, 'rb') as file:
             config = tomllib.load(file)
-            cls.config = munchify(config)
+        cls.config = Configuration(**config)
+
 
     class Database:
-        def __init__(self):
-            pass
 
-        @property
-        def getHost(self) -> str:
-            """Fetches values of host from config"""
-            return Config.config.DATABASE.host # type: ignore
+        @classmethod
+        def getHost(cls):
+            """Fetches values of Datbase.host from config"""
+            return Config.config.DATABASE.host
 
-        @property
-        def getPort(self) -> str:
-            """Fetches values of port from config"""
-            return Config.config.DATABASE.port # type: ignore
+        @classmethod
+        def getPort(cls) -> int:
+            """Fetches values of Datbase.port from config"""
+            return Config.config.DATABASE.port
 
-        @property
-        def getUsername(self) -> str:
-            """Fetches values of username from config"""
-            return Config.config.DATABASE.username # type: ignore
+        @classmethod
+        def getUsername(cls) -> str:
+            """Fetches values of Database.username from config"""
+            return Config.config.DATABASE.username
 
-        @property
-        def getPassword(self) -> str:
-            """Fetches values of username from config"""
-            return Config.config.DATABASE.password # type: ignore
+        @classmethod
+        def getPassword(cls) -> str:
+            """Fetches values of Database.username from config"""
+            return Config.config.DATABASE.password
 
 
     class Bot:
-        def __init__(self) -> None:
-            pass
 
-        @property
+        @classmethod
         def getToken(cls) -> str:
             """Gets bot token from config"""
+            return Config.config.DEFAULT.token
 
-            return Config.config.DEFAULT.token # type: ignore
-
-        @property
-        def getPrefix(cls) -> str:
-            """Gets bot prefix from config"""
-
-            return Config.config.DEFAULT.prefix # type: ignore
 
     class Version:
-        def __init__(self) -> None:
-            pass
 
-        @property
-        def getVersion(self) -> str:
+        @classmethod
+        def getVersion(cls) -> str:
             """Gets NateQK's Current version and compares to latest release"""
-            
 
-            CurrentVersion=Config.config.VERSION.version # type: ignore
+
+            CurrentVersion=Config.config.VERSION.version
             LatestVersion="0.0.1" # Ping github and look for latest version
             return CurrentVersion
 
@@ -74,13 +89,10 @@ class Config:
 
 if __name__ == "__main__":
     Config.loadConfig()
-
-    configBOT = Config.Bot()
-    configDB = Config.Database()
-
-    logger.info(configDB.getHost)
-    logger.info(configDB.getPort)
-    logger.info(configDB.getUsername)
-    logger.info(configDB.getPassword)
-    logger.info(configBOT.getToken)
-    logger.info(configBOT.getPrefix)
+    
+    logger.info(Config.Database.getHost())
+    logger.info(Config.Database.getPort())
+    logger.info(Config.Database.getUsername())
+    logger.info(Config.Database.getPassword())
+    logger.info(Config.Bot.getToken())
+    logger.info(Config.Version.getVersion())
