cf2ab5a11503a7a77510a8537adec7c4b738adea
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-10-23 00:04:18 -0700

    finished purge command

--- bot/extensions/purge.py
+++ bot/extensions/purge.py
@@ -1,24 +1,45 @@
 import hikari
 import arc
 import miru
+import datetime
+from loguru import logger
 
+from ..bot import BOT
 plugin: arc.GatewayPlugin = arc.GatewayPlugin("Purge")
 
 
 
 @plugin.include
 @arc.with_hook(arc.user_limiter(60, 1))
-@arc.slash_command("purge", "Purges a select amount of messages")
+@arc.with_hook(arc.has_permissions(hikari.Permissions.MANAGE_CHANNELS))
+@arc.slash_command("purge", "Purges a select amount of messages. Only works on messages younger than 14 days")
 async def purge(
     ctx: arc.GatewayContext,
-    purge_length: arc.Option[int, arc.IntParams(description="How many messages to Delete?", min=1, max=100)]
+    purge_length: arc.Option[int, arc.IntParams(description="How many messages to Delete?", min=1, max=500)]
 ) -> None:
-    
+
+    bulk_delete_limit: datetime.datetime = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=14)
     purgeEmbed: hikari.Embed = hikari.Embed(title="Deleted Messages", color='#f23914')
-    
-    purgeEmbed.add_field(name=f"Deleted messages", value=str(purge_length), inline=True)
-    
+    iterator = (
+        BOT.rest.fetch_messages(ctx.channel_id)
+        .take_while(lambda message: message.created_at > bulk_delete_limit)
+        .limit(purge_length)
+    )
+
+    await BOT.rest.delete_messages(ctx.channel_id, iterator)
     await ctx.respond(embed=purgeEmbed, flags=hikari.MessageFlag.EPHEMERAL)
+
+
+
+@purge.set_error_handler
+async def error_handler(ctx: arc.GatewayContext, exc: Exception) -> None:
+    logger.error(type(exc))
+    logger.error(exc)
+    if isinstance(exc, arc.errors.UnderCooldownError):
+        await ctx.respond(exc, flags=hikari.MessageFlag.EPHEMERAL)
+        return
+    
+
 
 
 @arc.loader
