fb18696056b189f9f5b263922fcce8deef30e6fa
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-10-23 20:49:23 -0700

    made ping command pretty

--- bot/extensions/ping.py
+++ bot/extensions/ping.py
@@ -9,11 +9,29 @@ from ..utils import configDB
 
 plugin: arc.GatewayPlugin = arc.GatewayPlugin("Ping")
 
+
+
+
 @plugin.include
 @arc.slash_command("ping", "Get's bot Latency to discord Servers")
 async def ping(ctx: arc.GatewayContext) -> None:
     """Responds to user calling command with bot latency to discord"""
-    await ctx.respond(f"Ping! Bot Latency is {str(BOT.heartbeat_latency*100)[:-10]} Ms")
+    botPing: float= float(str(BOT.heartbeat_latency*100)[:-10])
+    pingEmbed: hikari.Embed = hikari.Embed()
+    
+    pingEmbed.title = "Ping!"
+    if botPing < 35: pingEmbed.color = hikari.Color(int("15cdf2", 16))
+    elif botPing < 50: pingEmbed.color = hikari.Color(int("d2f884", 16))
+    elif botPing > 50: pingEmbed.color = hikari.Color(int("c73ea2"))
+    elif botPing > 200: pingEmbed.color = hikari.Color(int("ff0000"))
+
+    if botPing > 200:
+        pingEmbed.add_field(name="Latency!??!?!?!", value=f'{botPing} Ms', inline=True)
+        pingEmbed.add_field(name="WHY!!!!", value="Is your server running in a chinese basement???")
+    else:
+        pingEmbed.add_field(name="Latency", value=f'{botPing} Ms')
+    
+    await ctx.respond(embed=pingEmbed)
 
 
 @arc.loader
