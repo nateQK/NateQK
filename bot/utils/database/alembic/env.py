ce4423d53e53cb5043cb8186efe37d987be198d3
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-10-30 14:06:51 -0700

    added a fallback for models in alembic/env.py

--- bot/utils/database/alembic/env.py
+++ bot/utils/database/alembic/env.py
@@ -6,7 +6,10 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from ..models import Base
+try:
+    from bot.utils.database.models import Base
+except ModuleNotFoundError:
+    from models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
