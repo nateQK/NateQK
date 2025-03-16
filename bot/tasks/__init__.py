c1ee47b6775824c2a9055f36e6f974dc32842b82
KalebSchmidlkofer <kaleb@ashbyte.com>
2025-03-15 22:06:36 -0700

    fix: hourly task now doesn't fail

--- bot/tasks/__init__.py
+++ bot/tasks/__init__.py
@@ -1,9 +1,12 @@
 from os import path
-from bot.config import configuration as config
+from bot.config import Config
+
+config = Config.config
+
 
 async def hourlyTasks():
     """A list of Tasks that need to run every hour"""
-    from github import download_directory, update_directory
+    from bot.github import download_directory, update_directory
     if not path.exists(config.git.localdir):
         download_directory(config.git.repo)
 
